3ffc5ec9b4c5dd483338624279cbf4917d322985
Sameen Shah <smn.shah123@gmail.com>
2026-01-31 17:48:01 -0500

    Impl arena create and arena destroy

--- include/arena.h
+++ include/arena.h
@@ -5,7 +5,7 @@
 
 typedef struct Arena Arena;
 
-Arena* arena_create(size_t size);
+Arena* arena_create(size_t capacity);
 void arena_destroy(Arena *arena);
 void* arena_alloc(Arena* arena, size_t size);
 void arena_reset(Arena* arena);
--- src/arena.c
+++ src/arena.c
@@ -1,1 +1,37 @@
+#include "arena.h"
+#include <stdlib.h>
+
+struct Arena {
+	unsigned char *base;
+	size_t capacity;
+	size_t pos;
+};
+
+Arena* arena_create(size_t capacity) {
+	Arena* arena = malloc(sizeof *arena);
+	if (!arena) {
+		return NULL;
+	}
+	
+	arena->base = malloc(capacity);
+	if (!arena->base) {
+		free(arena);
+		return NULL;
+	}
+
+	arena->capacity = capacity;
+	arena->pos = 0;
+
+	return arena;
+}
+
+void arena_destroy(Arena *arena) {
+	if (!arena) {
+		return;
+	}
+
+	free(arena->base);
+	free(arena);
+} 
+
 
