b7719c9e00d21681237cfbef72252f8e398a9a5a
Sameen Shah <smn.shah123@gmail.com>
2026-01-31 18:52:17 -0500

    Impl arena alloc and arena reset

--- src/arena.c
+++ src/arena.c
@@ -1,6 +1,9 @@
 #include "arena.h"
 #include <stdlib.h>
 
+#define ARENA_ALIGN sizeof(void*)
+#define ALIGN_UP_POW2(val, align) (((val) + (align) - 1) & ~((align) - 1))
+
 struct Arena {
 	unsigned char *base;
 	size_t capacity;
@@ -9,8 +12,8 @@ struct Arena {
 
 Arena* arena_create(size_t capacity) {
 	Arena* arena = malloc(sizeof *arena);
-	if (!arena) {
-		return NULL;
+	if (!arena) { 
+		return NULL; 
 	}
 	
 	arena->base = malloc(capacity);
@@ -26,12 +29,36 @@ Arena* arena_create(size_t capacity) {
 }
 
 void arena_destroy(Arena *arena) {
-	if (!arena) {
-		return;
+	if (!arena) { 
+		return; 
 	}
 
 	free(arena->base);
 	free(arena);
-} 
+}
+
+void* arena_alloc(Arena *arena, size_t size) {
+	if (!arena) {
+		return NULL;
+	}
+
+	size_t pos_aligned = ALIGN_UP_POW2(arena->pos, ARENA_ALIGN);
+	size_t new_pos = pos_aligned + size;
+	
+	if (new_pos > arena->capacity) { 
+		return NULL; 
+	}
+
+	arena->pos = new_pos;
+	return arena->base + pos_aligned;
+}
+
+void arena_reset(Arena *arena) {
+	if (!arena) { 
+		return; 
+	}
+	
+	arena->pos = 0;
+}
 
 
